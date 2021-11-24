12ddef863a52f467dae6d385c1359586d0c92a3c
Chris Caballero <cmc17g@my.fsu.edu>
2021-11-24 12:27:13 -0500

    info command
    bpb and direntry struct.
    info parses bpb.

--- filesystem.c
+++ filesystem.c
@@ -6,17 +6,64 @@
 #include <fcntl.h>
 
 
+typedef struct {
+    unsigned short BPB_BytesPerSec;
+    unsigned char BPB_SecPerCluster;
+    unsigned short BPB_ReservedSecCnt;            // This holds the first FAT sector
+    unsigned char BPB_NumFATs;
+    unsigned int BPB_FATsize32;
+    unsigned int BPB_RootCluster;
+    unsigned int BPB_TotalSec32;
+} __attribute__((packed)) BPB_Info;
+
+struct DIRENTRY {
+    unsigned char DIR_Name[11];             // offset: 0
+    unsigned char DIR_Attributes;           // offset: 11
+    unsigned char DIR_NTRes;                // offset: 12
+    unsigned char DIR_CreatTimeTenth;       // offset: 13
+    unsigned short DIR_CreatTime;           // offset: 14 
+    unsigned short DIR_CreatDate;           // offset: 16
+    unsigned short DIR_LastAccessDate;      // offset: 18
+    unsigned short DIR_FirstClusterHI;      // offset: 20
+    unsigned short DIR_WriteTime;           // offset: 22
+    unsigned short DIR_WriteDate;           // offset: 24
+    unsigned short DIR_FirstClusterLO;      // offset: 26
+    unsigned short DIR_FileSize;            // offset: 28
+} __attribute__((packed));
+
+
+FILE *  img_file;
+BPB_Info BPB;
+// FSInfo FSI;
+// Environment ENV;
+
+
 char *get_input(void);
+
+
+int open_file(char* filename, char* mode) {
+    return 0;
+}
+int print_info(int offset);
 
 int main () {
 
 
     char *command; 
+    char *filename = "./fat32.img";
+
+    img_file = fopen(filename, "rb+");
+
+    print_info(0);
+
+
+    /*
     while(1) {
         printf("$ ");
         command = get_input();
 
         if(command == "exit") {
+            //release resources 
             break;
         } else if(command == "info") {
 
@@ -33,6 +80,9 @@ int main () {
         } else if(command == "mv") {
 
         } else if(command == "open") {
+            if(openFile() == -1) {
+                printf("Error");
+            }
 
         } else if(command == "close") {
 
@@ -51,9 +101,51 @@ int main () {
         }
 
     }
+    */
     
     return 0;
 }
+
+int print_info(int offset) {
+
+    fseek(img_file, 11, SEEK_SET);
+    fread(&BPB.BPB_BytesPerSec, 2, 1, img_file);
+    fread(&BPB.BPB_SecPerCluster, 1, 1, img_file);
+    fread(&BPB.BPB_ReservedSecCnt, 2, 1, img_file);
+    fread(&BPB.BPB_NumFATs, 1, 1, img_file);
+
+    fseek(img_file, 32, SEEK_SET);
+    fread(&BPB.BPB_TotalSec32, 4, 1, img_file);
+    fread(&BPB.BPB_FATsize32, 4, 1, img_file);
+    
+    fseek(img_file, 4, SEEK_CUR);
+    fread(&BPB.BPB_RootCluster, 4, 1, img_file);
+
+
+
+    printf("Bytes per Sector: %d\n", BPB.BPB_BytesPerSec);
+	printf("Sectors per Cluster: %d\n", BPB.BPB_SecPerCluster);
+    printf("Reserved Sector Count: %d\n", BPB.BPB_ReservedSecCnt);
+	printf("Number of FATs: %d\n", BPB.BPB_NumFATs);
+    printf("Total Sectors: %d\n", BPB.BPB_TotalSec32);  
+    printf("FAT Size: %d\n", BPB.BPB_FATsize32);
+    printf("Root Cluster: %d\n", BPB.BPB_RootCluster);
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 char *get_input(void) {
 	char *buffer = NULL;
